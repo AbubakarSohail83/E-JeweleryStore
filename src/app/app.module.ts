4a9130c8abb8f304ea7a975ad0f4d1cb40f37d03
Abubakar Sohail <abubakarsohail83@gmail.com>
2023-03-08 12:42:24 +0500

    deleted the test components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { Test1Component } from './test1/test1.component';
-import { Test2Component } from './test2/test2.component';
-
+ 
 @NgModule({
   declarations: [
-    AppComponent,
-    Test1Component,
-    Test2Component
-  ],
+    AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,

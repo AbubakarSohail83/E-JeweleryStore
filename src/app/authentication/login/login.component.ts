47050a21f56206c20800c4bb13fafc3bf4cf6a7d
Abubakar Sohail <abubakarsohail83@gmail.com>
2023-03-09 10:30:40 +0500

    Added a authentication folder and added login and forgetpassword

---
+++ src/app/authentication/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
